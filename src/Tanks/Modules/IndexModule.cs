532438b14bb916c78c7e69e81fde8967bb1e4f67
Krzysztof Morcinek <krzysztof.morcinek@smtsoftware.com>
2015-09-17 18:13:24 +0200

    Core & test projects

---
+++ src/Tanks/Modules/IndexModule.cs
@@ -0,0 +1,12 @@
+﻿using Nancy;
+
+namespace Tanks.Modules
+{
+    public class IndexModule : NancyModule
+    {
+        public IndexModule()
+        {
+            Get["/"] = _ => "Hello World!";
+        } 
+    }
+}
